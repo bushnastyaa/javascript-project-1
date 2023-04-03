2fb0cd365fe20881276cc4cfeb55c3a13cb37e2b
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-04-03 13:14:04 +0300

    Add mini-game brain-calc

---
+++ src/games/calc.js
@@ -0,0 +1,33 @@
+import startGame from '../index.js';
+
+const generateRandomNum = (max) => Math.floor(Math.random() * max);
+
+const calculate = (operand1, operand2, operator) => {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    default:
+      return operand1 * operand2;
+  }
+};
+
+const startCalcGame = () => {
+  const rules = 'What is the result of the expression?';
+
+  const getCorrectAnswer = () => {
+    const operators = ['+', '-', '*'];
+    const operator = operators[generateRandomNum(2)];
+    const firstNum = generateRandomNum(100);
+    const secondNum = generateRandomNum(100);
+
+    const question = `${firstNum} ${operator} ${secondNum}`;
+    const correctAnswer = String(calculate(firstNum, secondNum, operator));
+    return [question, correctAnswer];
+  };
+
+  startGame(rules, getCorrectAnswer);
+};
+
+export default startCalcGame;
